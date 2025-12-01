4e1c5ac1485277592705ccff4d0b1558daa43b87
Sofía García Juan <sgarjua@gmail.com>
2025-12-01 15:21:50 +0100

    Update

---
+++ GO-Compare.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+import sys
+from src.parser import Parser
+
+def main():
+    # 1. Definir los argumentos que acepta tu herramienta
+    args_def = {
+        ('-i', '--input'):  {'required': True, 'help': "Archivo de entrada"},
+        ('-o', '--output'): {'required': True, 'help': "Archivo de salida"},
+    }
+
+    # 2. Instanciar tu parser
+    my_parser = Parser(
+        description="Herramienta para análisis de anotaciones funcionales",
+        args=args_def
+    )
+
+    # 3. Parsear argumentos
+    args = my_parser.get().parse_args()
+
+    print(args)
+
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/parser.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import argparse
+from typing import Dict, Tuple
+
+class Parser:
+
+    def init(self, description: str, args: Dict[tuple, Dict]):
+        '''
+        Descripción: un string con una descripción.
+        argumentos: esto tiene su cosa; como lo haces tú es añadir primero el nombre corto del argumento, luego el largo
+        y finalmente, una serie de argumentos (que pueden llegar a ser variables). Para seguir la línea que has seguido 
+        y poder hacerlo genérico tienes que definir el parámetro argumentos así:
+
+        {
+            ('-i', '--input'): {'required': True, 'help':"Input FOF"}, 
+            ('-o', '--output'): {'required': True, 'help':"Output file name"}
+        }
+        '''
+
+        parser = argparse.ArgumentParser(description=description)
+        for key , value in args.items():
+            parser.add_argument(*key, **value)
+
+        self.parser_true = parser
+
+    def return_parser(self):
+        return self.parser_true
